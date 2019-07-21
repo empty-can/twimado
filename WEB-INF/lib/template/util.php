7708572b610f34711352db8c7de10dd8dcf21e81
empty-can <now.per.can@gmail.com>
2019-07-21 18:38:28 +0900

    PHPVerUP対応、引数空が許されなくなった問題

--- WEB-INF/lib/template/util.php
+++ WEB-INF/lib/template/util.php
@@ -11,12 +11,12 @@ function getMutters(string $api, array $parameters, $original_oldest_id) {
 	do {
 	    $tmp = getRequest($api, $parameters);
 	    $response = my_json_decode($tmp, true);
-	    
+
 	    if (! is_array($response)) {
 	        $errorMutter = new ErrorMutter();
 	        $errorMutter->addMessage("レスポンスの形式が不正でした。:"+var_export($tmp));
 	        $result['mutters'][] = obj_to_array($errorMutter);
-	        $result['oldest_mutter'] = new EmptyMutter();
+	        $result['oldest_mutter'] = new EmptyMutter("system");
 	        return $result;
         }
 
@@ -24,7 +24,7 @@ function getMutters(string $api, array $parameters, $original_oldest_id) {
             $errorMutter = new ErrorMutter();
             $errorMutter->addError($response['error']);
             $result['mutters'][] = obj_to_array($errorMutter);
-            $result['oldest_mutter'] = new EmptyMutter();
+            $result['oldest_mutter'] = new EmptyMutter("system");
             return $result;
         }
 
@@ -34,15 +34,15 @@ function getMutters(string $api, array $parameters, $original_oldest_id) {
         	$oldest_id = $response['oldest_mutter']['id'];
         else
             $oldest_id = - 1;
-            
+
         $parameters['max_id'] = $oldest_id;
 
     } while (count($response['mutters']) < 1 && $oldest_id > 0);
-    
+
     if($oldest_id != $original_oldest_id)
         $result['oldest_id'] = $oldest_id;
     else
         $result['oldest_id'] = -1;
-        
+
 	return $result;
 }
